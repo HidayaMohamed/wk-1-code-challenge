5cff5fb7a42ef2f24e486b46d5486ec32a2237da
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-09-09 21:21:19 +0300

    Formatted the grade-generator,js

--- grade-generator.js
+++ grade-generator.js
@@ -1,21 +1,20 @@
 let marks = prompt("Please enter marks:");
-console.log(marks)
+console.log(marks);
 
 if (marks > 100 || marks < 0) {
-    console.log("Marks invalid.")
+  console.log("Marks invalid.");
 } else if (marks >= 79) {
-     console.log("Grade = A ")
-} else if(marks >= 60){
-     console.log("Grade =B")
+  console.log("Grade = A ");
+} else if (marks >= 60) {
+  console.log("Grade =B");
 } else if (marks >= 49) {
-     console.log("Grade = C")
+  console.log("Grade = C");
 } else if (marks >= 40) {
-     console.log("Grade = D")
-} else if(marks <40 ){
-     console.log("Grade = E")
+  console.log("Grade = D");
+} else if (marks < 40) {
+  console.log("Grade = E");
 } else {
-    console.log("input marks")
+  console.log("input marks");
 }
 
 console.log(marks);
-
