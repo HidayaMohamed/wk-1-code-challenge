ab6d36a2365dcf5ae340f1ff94d11f7be69bca94
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-09-10 04:13:00 +0300

    Adds else if to the net-salary=calculator.js

--- net-salary-calculator.js
+++ net-salary-calculator.js
@@ -1,3 +1,7 @@
 function netSalary(basicSalary, benefits) {
-    
+    if (basicSalary <= 24000) {
+        tax = basicSalary *0.1
+    } else if (basicSalary <= 32, 333) {
+        tax = basicSalary * 0.25
+} esle if (){}
 }
